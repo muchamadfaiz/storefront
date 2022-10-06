1d31dbb7f05425f2dd3c2e9d490018a3a92c48ca
muchamadfaiz <muchamadfaiz@gmail.com>
2022-10-06 14:52:59 +0700

    choice field

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def say_hello (request):
     # return HttpResponse('Hello World')
     return render(request, 'hello.html',{'name': 'Faiz'})
