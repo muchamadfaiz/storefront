1116b062eeaa8f963a9475efd81eb08ad492affc
muchamadfaiz <muchamadfaiz@gmail.com>
2022-10-06 16:28:11 +0700

    circular relationship

--- store/models.py
+++ store/models.py
@@ -1,22 +1,21 @@
-from email.errors import MalformedHeaderDefect
 from django.db import models
 
-# Create your models here.
+
 class Promotion (models.Model):
     description = models.CharField(max_length=255)
     discount = models.FloatField()
 
 class Collection (models.Model):
     title = models.CharField(max_length=255)
+    featured_product = models.ForeignKey(
+        'Product', on_delete=models.SET_NULL, null=True, related_name ='+')
     
 class Product(models.Model):
-    # sku = models.Charfield(max_length=10, primary_key=True)
     title = models.CharField(max_length =255)
     description = models.TextField()
-    # max price 9999.99 
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
-    last_update = models.DateTmeField(auto_now=True)
+    last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
     promotions = models.ManyToManyField(Promotion)
 
@@ -24,6 +23,7 @@ class Customer(models.Model):
     MEMBERSHIP_BRONZE = 'B'
     MEMBERSHIP_SILVER = 'S'
     MEMBERSHIP_GOLD = 'G'
+
     MEMBERSHIP_CHOICES = [
         (MEMBERSHIP_BRONZE, 'Bronze'),
         (MEMBERSHIP_SILVER, 'Silver'),
@@ -33,8 +33,10 @@ class Customer(models.Model):
     last_name = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
     phone = models.IntegerField()
-    birth_date = models.Datefield(null=True)
-    membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+    birth_date = models.DateField(null=True)
+    membership = models.CharField(
+        max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+
 
 class Order(models.Model):
     PAYMENT_STATUS_PENDING = 'P'
@@ -45,25 +47,32 @@ class Order(models.Model):
         (PAYMENT_STATUS_COMPLETE, 'Complete'),
         (PAYMENT_STATUS_PENDING, 'Failed'),
     ]
+
     placed_at = models.DateTimeField(auto_now_add=True)
-    payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
+    payment_status = models.CharField(
+        max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
 
+
 class OrderItem (models.Model):
-    quantity = models.ForeignKey(Order, on_delete=models.PROTECT)
-    unit_price = models.PositiveSmallIntegerField()
+    quantity = models.PositiveIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     order = models.ForeignKey(Order, on_delete=models.PROTECT)
     product = models.ForeignKey(Product, on_delete=models.PROTECT)
     
+
 class Address (models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
-    customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)
+    customer = models.OneToOneField(
+        Customer, on_delete=models.CASCADE)
+
 
 class Cart (models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
+
 class CartItem (models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)  
-    product = models.ForeignKey(Cart, on_delete=models.CASCADE)  
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)  
     quantity = models.PositiveSmallIntegerField()  
