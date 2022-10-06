18cdf018d630b02a7dbdb4cbbbc77d96d9a793e0
muchamadfaiz <muchamadfaiz@gmail.com>
2022-10-06 15:42:43 +0700

    many to many relationship

--- store/models.py
+++ store/models.py
@@ -2,6 +2,10 @@ from email.errors import MalformedHeaderDefect
 from django.db import models
 
 # Create your models here.
+class Promotion (models.Model):
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
+
 class Collection (models.Model):
     title = models.CharField(max_length=255)
     
@@ -14,6 +18,7 @@ class Product(models.Model):
     inventory = models.IntegerField()
     last_update = models.DateTmeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
+    promotions = models.ManyToManyField(Promotion)
 
 class Customer(models.Model):
     MEMBERSHIP_BRONZE = 'B'
